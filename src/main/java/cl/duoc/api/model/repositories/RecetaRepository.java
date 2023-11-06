47aa3d33ba1221df78da610e174a2c243dfa5e62
claudiosanchezt <107229431+claudiosanchezt@users.noreply.github.com>
2023-11-05 21:06:22 -0300

    ms-recetas-1-0

---
+++ src/main/java/cl/duoc/api/model/repositories/RecetaRepository.java
@@ -0,0 +1,11 @@
+package cl.duoc.api.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.duoc.api.model.entities.Receta;
+
+import java.util.List;
+
+public interface RecetaRepository extends JpaRepository<Receta, Integer>{
+    List<Receta> findByNombre(String nombre);
+}
